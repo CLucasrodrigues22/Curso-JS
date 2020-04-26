20829b4bfddfbdf58956f2a033c6a600b7d3e3d2
CLucasrodrigues22 <c.lucasrodrigues22@gmail.com>
2020-04-26 17:23:49 -0300

    testando a tipagem do js

---
+++ tipagemfraca.js
@@ -0,0 +1,11 @@
+//JavaScript é uma linguagem de tipagem fraca
+let qualquer = 'Legal'
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 345
+
+console.log(qualquer)
+console.log(typeof qualquer)
+
+//Evite nome generico e siglas
